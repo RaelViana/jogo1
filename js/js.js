f460b542016a18bf9f368027aa27b49610c1c5e0
RaelViana <catedralviana@hotmail.com>
2020-11-12 11:58:38 -0300

    criando disparo do game

--- js/js.js
+++ js/js.js
@@ -8,7 +8,8 @@
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     
     //Principais variáveis do jogo
-	
+
+	var podeAtirar=true;
     var jogo = {}
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
@@ -83,7 +84,8 @@
 	
 	if (jogo.pressionou[TECLA.D]) {
 		
-		//Chama função Disparo	
+        //Chama função Disparo
+        disparo();	
 	}
 
     } // fim da função movejogador()
@@ -125,6 +127,40 @@
             }
     
     } // fim da função moveamigo()
+
+    
+function disparo() {
+	
+	if (podeAtirar==true) {
+		
+	podeAtirar=false;
+	
+	topo = parseInt($("#jogador").css("top"))
+	posicaoX= parseInt($("#jogador").css("left"))
+	tiroX = posicaoX + 190;
+	topoTiro=topo+37;
+	$("#fundoGame").append("<div id='disparo'></div");
+	$("#disparo").css("top",topoTiro);
+	$("#disparo").css("left",tiroX);
+	
+	var tempoDisparo=window.setInterval(executaDisparo, 30);
+	
+	} //Fecha podeAtirar
+ 
+   	    function executaDisparo() {
+	    posicaoX = parseInt($("#disparo").css("left"));
+	    $("#disparo").css("left",posicaoX+15); 
+
+        		if (posicaoX>900) {
+						
+			window.clearInterval(tempoDisparo);
+			tempoDisparo=null;
+			$("#disparo").remove();
+			podeAtirar=true;
+					
+                   }
+	} // Fecha executaDisparo()
+    } // Fecha disparo()
     
 
 
