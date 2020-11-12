c2bb0a8bb24e22be07923e00fb3a19f527e5bb91
RaelViana <catedralviana@hotmail.com>
2020-11-12 11:48:53 -0300

    movimentando o amigo

--- js/js.js
+++ js/js.js
@@ -41,6 +41,7 @@
     movejogador();
     moveinimigo1();
     moveinimigo2();
+    moveamigo();
 	
     } // Fim da função loop()
     
@@ -111,6 +112,20 @@
 					
 		}
     } // Fim da função moveinimigo2()
+
+    function moveamigo() {
+	
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX+1);
+                    
+            if (posicaoX>906) {
+                
+            $("#amigo").css("left",0);
+                        
+            }
+    
+    } // fim da função moveamigo()
+    
 
 
     } // Fim da função start
