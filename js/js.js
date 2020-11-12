5d005ca36cb2a5d86c78fe5cf4171f804df9410c
RaelViana <catedralviana@hotmail.com>
2020-11-11 22:27:01 -0300

    criando o game looping do jogo

--- js/js.js
+++ js/js.js
@@ -5,6 +5,30 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    
+    //Principais variáveis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+    } // Fim da função loop()
+    
+    
+//Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position",esquerda-1); /* função anda 1 px para esquerda*/
+        
+        } // fim da função movefundo()
 
 } // Fim da função start
